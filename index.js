6518b554c228d2707acf5f6b413270aa3e41d13a
Rachit2833 <rrachit346@gmail.com>
2024-09-08 17:11:46 +0530

    service worker final

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
-if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('./sw.js').then(reg => {
-            console.log('Registration successful', reg);
-        })
-            .catch(e => console.error('Error during service worker registration:', e));
-    } else {
-     console.warn('Service Worker is not supported');
-    }
-
-   
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js", { scope: "/" })
+    .then((registration) => {
+      console.log("Service Worker registered with scope:", registration.scope,registration);
+    })
+    .catch((error) => {
+      console.error("Service Worker registration failed:", error);
+    });
+}
