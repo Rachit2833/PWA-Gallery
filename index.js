7e65efefb0ce94f483b89641fb5b18184eb39ebf
Rachit2833 <rrachit346@gmail.com>
2024-09-08 15:24:42 +0530

    chnages in index.jsl

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+
 if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js').then(reg => {
+        navigator.serviceWorker.register('./sw.js').then(reg => {
             console.log('Registration successful', reg);
         })
             .catch(e => console.error('Error during service worker registration:', e));
